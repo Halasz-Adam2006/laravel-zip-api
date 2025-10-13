0d9aef6d6dfc9b9f611f050ffde998132a71e6c4
H.Ádám <adamukieger@gmail.com>
2025-10-13 10:01:51 +0200

    api login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json(['token' => $token]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,11 @@ use App\Http\Controllers\PostalCodeController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-});
+})->middleware('auth:sanctum');
 Route::get('postal-codes', [PostalCodeController::class, 'index']);
 Route::get('postal-codes/{postal_code}', [PostalCodeController::class, 'show']);
-/*Route::middleware('auth:sanctum')->group(function () {
-    Route::post('postal-codes', [PostalCodeController::class, 'store']);
-    Route::put('postal-codes/{postal_code}', [PostalCodeController::class, 'update']);
-    Route::delete('postal-codes/{postal_code}', [PostalCodeController::class, 'destroy']);
-});*/
+route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
 
-Route::post('postal-codes', [PostalCodeController::class, 'store']);
-Route::put('postal-codes/{postal_code}', [PostalCodeController::class, 'update']);
-Route::delete('postal-codes/{postal_code}', [PostalCodeController::class, 'destroy']);
+Route::post('postal-codes', [PostalCodeController::class, 'store']) ->middleware('auth:sanctum');
+Route::put('postal-codes/{postal_code}', [PostalCodeController::class, 'update']) ->middleware('auth:sanctum');
+Route::delete('postal-codes/{postal_code}', [PostalCodeController::class, 'destroy']) ->middleware('auth:sanctum');
